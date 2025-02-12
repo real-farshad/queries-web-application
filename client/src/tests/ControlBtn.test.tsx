77fc81ba907040e762e51897ecef514e4376a6e0
Farshad <m0hammadr3za.dev@gmail.com>
2025-02-12 04:06:40 +0330

    📦 update packages

--- client/src/tests/ControlBtn.test.tsx
+++ client/src/tests/ControlBtn.test.tsx
@@ -5,31 +5,31 @@ const mockType = "next";
 const mockSize = "large";
 
 it("should redner without any errors", () => {
-    render(<ControlBtn />);
+  render(<ControlBtn />);
 });
 
 describe("ControlBtn", () => {
-    it("should have prev text in button if no type is passed", () => {
-        render(<ControlBtn />);
-        const prevBtn = screen.getByText(/prev/i);
-        expect(prevBtn).toBeInTheDocument();
-    });
+  it("should have prev text in button if no type is passed", () => {
+    render(<ControlBtn />);
+    const prevBtn = screen.getByText(/prev/i);
+    expect(prevBtn).toBeInTheDocument();
+  });
 
-    it("should have next text in button if button type is next", () => {
-        render(<ControlBtn type={mockType} />);
-        const nextBtn = screen.getByText(/next/i);
-        expect(nextBtn).toBeInTheDocument();
-    });
+  it("should have next text in button if button type is next", () => {
+    render(<ControlBtn type={mockType} />);
+    const nextBtn = screen.getByText(/next/i);
+    expect(nextBtn).toBeInTheDocument();
+  });
 
-    it("should NOT have large testid if button size is not passed", () => {
-        render(<ControlBtn />);
-        const largeStyling = screen.queryByTestId("large");
-        expect(largeStyling).not.toBeInTheDocument();
-    });
+  it("should NOT have large testid if button size is not passed", () => {
+    render(<ControlBtn />);
+    const largeStyling = screen.queryByTestId("large");
+    expect(largeStyling).not.toBeInTheDocument();
+  });
 
-    it("should have large testid if button size is large", () => {
-        render(<ControlBtn size={mockSize} />);
-        const largeStyling = screen.getByTestId("large");
-        expect(largeStyling).toBeInTheDocument();
-    });
+  it("should have large testid if button size is large", () => {
+    render(<ControlBtn size={mockSize} />);
+    const largeStyling = screen.getByTestId("large");
+    expect(largeStyling).toBeInTheDocument();
+  });
 });
