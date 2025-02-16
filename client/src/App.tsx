55fabb9238d5f19d965fbf698a9e55f4c91c4268
Farshad <m0hammadr3za.dev@gmail.com>
2025-02-16 09:54:03 +0330

    ✨ add github actions ci/cd pipeline

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,9 @@
 import { Fragment, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { selectPageLoading, finishPageLoading } from "./redux/slices/loadingSlice";
+import {
+  selectPageLoading,
+  finishPageLoading,
+} from "./redux/slices/loadingSlice";
 import { loadFavorites } from "./redux/slices/favoritesSlice";
 import { changeNumberOfPages, loadPosts } from "./redux/slices/postsSlice";
 import LoadingScreen from "./components/LoadingScreen";
@@ -14,53 +17,54 @@ import Footer from "./components/Footer";
 import "./styles/App.scss";
 
 function App() {
-    const pageLoading = useSelector(selectPageLoading);
+  const pageLoading = useSelector(selectPageLoading);
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        (async () => {
-            const favoritesRes = await fetch("/api/favorites");
-            const favorites = await favoritesRes.json();
-            dispatch(loadFavorites(favorites));
+  useEffect(() => {
+    (async () => {
+      const favoritesRes = await fetch("/api/favorites");
+      const favorites = await favoritesRes.json();
+      dispatch(loadFavorites(favorites));
 
-            // check for number of posts in database
-            const postsCountRes = await fetch("/api/posts/count");
-            const postsCount = await postsCountRes.json();
-            const numberOfPages = Math.ceil(postsCount / 4);
-            dispatch(changeNumberOfPages(numberOfPages));
+      // check for number of posts in database
+      const postsCountRes = await fetch("/api/posts/count");
+      const postsCount = await postsCountRes.json();
+      const numberOfPages = Math.ceil(postsCount / 4);
+      dispatch(changeNumberOfPages(numberOfPages));
 
-            // fetch latest 4 posts
-            const postsRes = await fetch("/api/posts");
-            const posts = await postsRes.json();
-            dispatch(loadPosts(posts));
+      // fetch latest 4 posts
+      const postsRes = await fetch("/api/posts");
+      const posts = await postsRes.json();
+      dispatch(loadPosts(posts));
 
-            // stop page loading after 1s so that the images will be loaded
-            setTimeout(() => dispatch(finishPageLoading()), 1000);
-        })();
-    }, []);
+      // stop page loading after 1s so that the images will be loaded
+      setTimeout(() => dispatch(finishPageLoading()), 1000);
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    return (
-        <Fragment>
-            {pageLoading && <LoadingScreen />}
+  return (
+    <Fragment>
+      {pageLoading && <LoadingScreen />}
 
-            <Header />
+      <Header />
 
-            <ContentContainer>
-                <main>
-                    <Posts />
+      <ContentContainer>
+        <main>
+          <Posts />
 
-                    <Community />
+          <Community />
 
-                    <Challenges />
+          <Challenges />
 
-                    <Tools />
-                </main>
+          <Tools />
+        </main>
 
-                <Footer />
-            </ContentContainer>
-        </Fragment>
-    );
+        <Footer />
+      </ContentContainer>
+    </Fragment>
+  );
 }
 
 export default App;
--- client/src/components/Intro.tsx
+++ client/src/components/Intro.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import { selectFavorites } from "../redux/slices/favoritesSlice";
@@ -6,143 +7,139 @@ import ControlBtn from "./ControlBtn";
 import "../styles/Intro.scss";
 
 function Intro() {
-    // Mock Data
-    const favorites: any = useSelector(selectFavorites);
+  // Mock Data
+  const favorites: any = useSelector(selectFavorites);
 
-    const favoritesRef: any = useRef(null);
-    const [slideIndex, setSlideIndex] = useState(0);
-    const [touchStartPosition, setTouchStartPosition] = useState(0);
+  const favoritesRef: any = useRef(null);
+  const [slideIndex, setSlideIndex] = useState(0);
+  const [touchStartPosition, setTouchStartPosition] = useState(0);
 
-    // windowWidth can be sm, md or xl
-    const [windowWidth, setWindowWidth] = useState(getWindowWidth());
+  // windowWidth can be sm, md or xl
+  const [windowWidth, setWindowWidth] = useState(getWindowWidth());
 
-    // change slider position on page and resize change
-    useEffect(() => {
-        if (!favoritesRef) return;
-        const cardsLeftPosition = calculateCardsLeftPosition();
-        favoritesRef.current.style.transform = `translateX(-${cardsLeftPosition}%)`;
-    }, [slideIndex, windowWidth]);
+  // change slider position on page and resize change
+  useEffect(() => {
+    if (!favoritesRef) return;
+    const cardsLeftPosition = calculateCardsLeftPosition();
+    favoritesRef.current.style.transform = `translateX(-${cardsLeftPosition}%)`;
+  }, [slideIndex, windowWidth]);
 
-    // handle resize changes and layout shift on different screen sizes
-    useEffect(() => {
-        window.addEventListener("resize", handleResize);
-        return () => window.removeEventListener("resize", handleResize);
-    }, [slideIndex, windowWidth]);
+  // handle resize changes and layout shift on different screen sizes
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [slideIndex, windowWidth]);
 
-    function handleClickOnPrev() {
-        if (slideIndex === 0) return;
-        setSlideIndex((prevState) => prevState - 2);
+  function handleClickOnPrev() {
+    if (slideIndex === 0) return;
+    setSlideIndex((prevState) => prevState - 2);
+  }
+
+  function handleClickOnNext() {
+    if (slideIndex + 2 > 5) return;
+    setSlideIndex((prevState) => prevState + 2);
+  }
+
+  function handleTouchStart(e: any) {
+    setTouchStartPosition(e.touches[0].clientX);
+  }
+
+  function handleTouchEnd(e: any) {
+    // swipe only works for small screens
+    if (windowWidth !== "sm") return;
+
+    const positionDifference = touchStartPosition - e.changedTouches[0].clientX;
+    const minDifferenceForTouch = 40;
+
+    if (positionDifference <= -minDifferenceForTouch && slideIndex !== 0) {
+      setSlideIndex((prevState) => prevState - 1);
+    } else if (positionDifference >= minDifferenceForTouch && slideIndex < 5) {
+      setSlideIndex((prevState) => prevState + 1);
     }
+  }
 
-    function handleClickOnNext() {
-        if (slideIndex + 2 > 5) return;
-        setSlideIndex((prevState) => prevState + 2);
+  function getWindowWidth() {
+    const windowWidth = window.innerWidth;
+
+    // translate window with to sm, md or xl
+    if (windowWidth < 768) return "sm";
+    if (windowWidth < 1200) return "md";
+    return "xl";
+  }
+
+  function calculateCardsLeftPosition() {
+    // slides left position on sm
+    const smCardsPosition = [0, 93, 186, 279, 372, 465];
+    if (windowWidth === "sm") return smCardsPosition[slideIndex];
+
+    // slides left position on md
+    const mdCardsPosition = [0, 104, 208];
+    if (windowWidth === "md") return mdCardsPosition[slideIndex / 2];
+
+    // slides left position on xl
+    const xlCardsPosition = [0, 102.5, 205];
+    return xlCardsPosition[slideIndex / 2];
+  }
+
+  function handleResize() {
+    const newWindowWidth = getWindowWidth();
+
+    if (windowWidth !== newWindowWidth) {
+      let newSlideIndex;
+
+      const sliderLayoutIsStillSame =
+        (windowWidth === "xl" && newWindowWidth === "md") ||
+        (windowWidth === "md" && newWindowWidth === "xl");
+
+      if (newWindowWidth === "sm" || sliderLayoutIsStillSame) {
+        newSlideIndex = slideIndex;
+      } else {
+        newSlideIndex = slideIndex % 2 === 0 ? slideIndex : slideIndex - 1;
+      }
+
+      setWindowWidth(newWindowWidth);
+      setSlideIndex(newSlideIndex);
     }
+  }
 
-    function handleTouchStart(e: any) {
-        setTouchStartPosition(e.touches[0].clientX);
-    }
-
-    function handleTouchEnd(e: any) {
-        // swipe only works for small screens
-        if (windowWidth !== "sm") return;
-
-        const positionDifference = touchStartPosition - e.changedTouches[0].clientX;
-        const minDifferenceForTouch = 40;
-
-        if (positionDifference <= -minDifferenceForTouch && slideIndex !== 0) {
-            setSlideIndex((prevState) => prevState - 1);
-        } else if (positionDifference >= minDifferenceForTouch && slideIndex < 5) {
-            setSlideIndex((prevState) => prevState + 1);
-        }
-    }
-
-    function getWindowWidth() {
-        const windowWidth = window.innerWidth;
-
-        // translate window with to sm, md or xl
-        if (windowWidth < 768) return "sm";
-        if (windowWidth < 1200) return "md";
-        return "xl";
-    }
-
-    function calculateCardsLeftPosition() {
-        // slides left position on sm
-        const smCardsPosition = [0, 93, 186, 279, 372, 465];
-        if (windowWidth === "sm") return smCardsPosition[slideIndex];
-
-        // slides left position on md
-        const mdCardsPosition = [0, 104, 208];
-        if (windowWidth === "md") return mdCardsPosition[slideIndex / 2];
-
-        // slides left position on xl
-        const xlCardsPosition = [0, 102.5, 205];
-        return xlCardsPosition[slideIndex / 2];
-    }
-
-    function handleResize() {
-        const newWindowWidth = getWindowWidth();
-
-        if (windowWidth !== newWindowWidth) {
-            let newSlideIndex;
-
-            const sliderLayoutIsStillSame =
-                (windowWidth === "xl" && newWindowWidth === "md") ||
-                (windowWidth === "md" && newWindowWidth === "xl");
-
-            if (newWindowWidth === "sm" || sliderLayoutIsStillSame) {
-                newSlideIndex = slideIndex;
-            } else {
-                newSlideIndex = slideIndex % 2 === 0 ? slideIndex : slideIndex - 1;
-            }
-
-            setWindowWidth(newWindowWidth);
-            setSlideIndex(newSlideIndex);
-        }
-    }
-
-    return (
-        <div className="intro">
-            <div
-                className="intro__favorites-container"
-                onTouchStart={handleTouchStart}
-                onTouchEnd={handleTouchEnd}
-            >
-                <div className="intro__favorites" ref={favoritesRef}>
-                    {favorites.length > 0 &&
-                        favorites.map((data: any, index: number) => {
-                            return (
-                                <a
-                                    href="/#"
-                                    className="intro__favorite-card"
-                                    key={data._id}
-                                >
-                                    <FavoriteCard data={data} count={index + 1} />
-                                </a>
-                            );
-                        })}
-                </div>
-            </div>
-            <div className="intro__controlls">
-                <div
-                    className={`intro__prev-btn ${
-                        slideIndex > 0 ? " intro__prev-btn--active" : ""
-                    }`}
-                    onClick={handleClickOnPrev}
-                >
-                    <ControlBtn size="large" />
-                </div>
-                <div
-                    className={`intro__next-btn ${
-                        !(slideIndex + 2 >= 6) ? " intro__next-btn--active" : ""
-                    }`}
-                    onClick={handleClickOnNext}
-                >
-                    <ControlBtn type="next" size="large" />
-                </div>
-            </div>
+  return (
+    <div className="intro">
+      <div
+        className="intro__favorites-container"
+        onTouchStart={handleTouchStart}
+        onTouchEnd={handleTouchEnd}
+      >
+        <div className="intro__favorites" ref={favoritesRef}>
+          {favorites.length > 0 &&
+            favorites.map((data: any, index: number) => {
+              return (
+                <a href="/#" className="intro__favorite-card" key={data._id}>
+                  <FavoriteCard data={data} count={index + 1} />
+                </a>
+              );
+            })}
         </div>
-    );
+      </div>
+      <div className="intro__controlls">
+        <div
+          className={`intro__prev-btn ${
+            slideIndex > 0 ? " intro__prev-btn--active" : ""
+          }`}
+          onClick={handleClickOnPrev}
+        >
+          <ControlBtn size="large" />
+        </div>
+        <div
+          className={`intro__next-btn ${
+            !(slideIndex + 2 >= 6) ? " intro__next-btn--active" : ""
+          }`}
+          onClick={handleClickOnNext}
+        >
+          <ControlBtn type="next" size="large" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Intro;
